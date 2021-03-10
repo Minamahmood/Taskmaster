261a1d29bc325af4423f5f6a3d55a9b1bc627831
minamahmood83 <minamahmood83@gmail.com>
2021-03-09 22:43:22 -0800

    cleaning up concolelog

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,20 +7,20 @@ function styleTime() {
     $("#theDate").append(`<br>${curTime}`);
     curTime = moment().format("H");
     curTime -= 9;
-    console.log(curTime);
+    // console.log(curTime);
     for (let i = 0; i <= 18; i++) {
         if (i < curTime) {
             //past
-            console.log(i);
+            //console.log(i);
             $("#" + i).attr("style", "background-color:lightpink");
             $("#" + i).attr("readonly", true);
         } else if (i === curTime) {
             //present
-            console.log(i);
+            // console.log(i);
             $("#" + i).attr("style", "background-color:green");
         } else {
             //future
-            console.log(i);
+            //console.log(i);
             $("#" + i).attr("style", "background-color:white");
         }
     }
@@ -31,16 +31,16 @@ $(".rowBtn").on("click", function() {
     //get the value of its parent input
     //then saves it to localstorige
     var currentTime = $(this).parent().attr("id");
-    console.log($($(this).parent().children()[1]).val());
+    //console.log($($(this).parent().children()[1]).val());
     var dayPlan = $($(this).parent().children()[1]).val();
     localStorage.setItem(currentTime, dayPlan);
 });
 $(".time").each(function() {
-    console.log($(this));
+    // console.log($(this));
     var currentTime = $(this).parent().attr("id");
-    console.log($(this).parent().attr("id"));
+    //console.log($(this).parent().attr("id"));
 
     var dayPlan = localStorage.getItem(currentTime);
     $(this).val(dayPlan);
-    console.log($(this).val(dayPlan));
+    //console.log($(this).val(dayPlan));
 });
