72241240cc5bbad987aec251e3bb80b4e51a5041
minamahmood83 <minamahmood83@gmail.com>
2021-03-09 21:21:59 -0800

    setup time logic

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,18 @@
 var curDate = moment().format("MM/DD/YYYY");
 $("#theDate").append(curDate);
-// var timeArr = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+styleTime();
+
+function styleTime() {
+    var curTime = moment().format("h A");
+    $("#theDate").append(`<br>${curTime}`);
+    curTime = moment().format("H");
+    for (i = 0; i <= 18; i++) {
+        if (i < curTime) {
+            //past
+        } else if (i === curTime) {
+            //present
+        } else {
+            //future
+        }
+    }
+}
